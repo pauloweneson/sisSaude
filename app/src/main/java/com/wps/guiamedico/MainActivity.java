b0bb45ceac6deb6bec266f8eae55a82ca3342a8d
Paulo <paulowschaves@gmail.com>
2017-08-23 19:49:06 -0300

    Comitando novas coisas

--- app/src/main/java/com/wps/guiamedico/MainActivity.java
+++ app/src/main/java/com/wps/guiamedico/MainActivity.java
@@ -1,15 +1,29 @@
 package com.wps.guiamedico;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.TextureView;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
     private TextView linkCadastrar;
+    private EditText email;
+    private EditText senha;
+    private FirebaseAuth mAuth;
+    private TextView linkEsqueci;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +40,32 @@ public class MainActivity extends AppCompatActivity {
         Intent telaCadastro = new Intent(MainActivity.this, cadastroActivity.class);
         startActivity(telaCadastro);
     }
-    public void linkEsqueciOnClick(View v) {
-        Intent telaEsqueci = new Intent(MainActivity.this, esqueciActivity.class);
-        startActivity(telaEsqueci);
+    public void esqueci() {
+        linkEsqueci.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent telaEsqueci = new Intent(MainActivity.this, esqueciActivity.class);
+                startActivity(telaEsqueci);
+            }
+        });
+
+    }
+    private void validarLogin(){
+        email = (EditText)findViewById(R.id.tb_email);
+        senha = (EditText)findViewById(R.id.tb_senha);
+        mAuth = FirebaseAuth.getInstance();
+        mAuth.signInWithEmailAndPassword(email.getText().toString(),senha.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent chamaTela = new Intent(MainActivity.this,indexActivity.class);
+                    startActivity(chamaTela);
+                    Toast.makeText(MainActivity.this,"Login efetuado com sucesso!", Toast.LENGTH_SHORT).show();
+                    finish();
+                }else{
+                    Toast.makeText(MainActivity.this,"Usuário ou senha incorretos!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/wps/guiamedico/esqueciActivity.java
+++ app/src/main/java/com/wps/guiamedico/esqueciActivity.java
@@ -2,11 +2,14 @@ package com.wps.guiamedico;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class esqueciActivity extends AppCompatActivity {
@@ -21,7 +24,7 @@ public class esqueciActivity extends AppCompatActivity {
     }
 
     public void btVoltarOnClick(View v) {
-        Intent telaLogin = new Intent(this,MainActivity.class);
+        Intent telaLogin = new Intent(this, MainActivity.class);
         startActivity(telaLogin);
     }
 
@@ -29,18 +32,25 @@ public class esqueciActivity extends AppCompatActivity {
         Funcoes funcoes = new Funcoes();
         tbEmail = (EditText) findViewById(R.id.tb_email);
 
-        if(!tbEmail.getText().toString().equals("")) {
+        if (!tbEmail.getText().toString().equals("")) {
 
-                mAuth = FirebaseAuth.getInstance();
-                mAuth
-                        .sendPasswordResetEmail(tbEmail.getText().toString());
-                Toast.makeText(getApplicationContext(),"E-MAIL ENVIADO COM SUCESSO", Toast.LENGTH_SHORT).show();
-                finish();
-            }
-            else{
-                funcoes.alerta(this,"Alerta","Dado não inserido.");
-            }
+            mAuth = FirebaseAuth.getInstance();
+            mAuth
+                    .sendPasswordResetEmail(tbEmail.getText().toString())
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                Toast.makeText(getApplicationContext(), "E-MAIL ENVIADO COM SUCESSO", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+            finish();
+        } else {
+            Toast.makeText(getApplicationContext(), "E-MAIL NÃO ENVIADO COM SUCESSO", Toast.LENGTH_SHORT).show();
         }
 
-
     }
+
+
+}
