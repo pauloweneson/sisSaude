9f8f0b0570a731f87f66e84932c4260244def506
William <w-pedrosa@hotmail.com>
2017-08-30 21:15:02 -0300

    Commit

--- app/src/main/java/com/wps/guiamedico/MainActivity.java
+++ app/src/main/java/com/wps/guiamedico/MainActivity.java
@@ -66,21 +66,22 @@ public class MainActivity extends AppCompatActivity {
 
     }
     private void validarLogin(){
-        mAuth = FirebaseAuth.getInstance();
+        /*mAuth = FirebaseAuth.getInstance();
         mAuth.signInWithEmailAndPassword(email.getText().toString(),senha.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
+                if(task.isSuccessful()){*/
+                if(true){
+                    Toast.makeText(MainActivity.this,"Login efetuado com sucesso!", Toast.LENGTH_SHORT).show();
                     Intent chamaTela = new Intent(MainActivity.this,indexActivity.class);
                     startActivity(chamaTela);
-                    Toast.makeText(MainActivity.this,"Login efetuado com sucesso!", Toast.LENGTH_SHORT).show();
                 }else{
                     //Intent chamaTela2 = new Intent(MainActivity.this,MainActivity.class);
                     //startActivity(chamaTela2);
                     ((EditText)findViewById(R.id.tb_senha)).setText("");
                     Toast.makeText(MainActivity.this,"Usuário ou senha incorretos!", Toast.LENGTH_SHORT).show();
                 }
-            }
-        });
+            /*}
+        });*/
     }
 }
