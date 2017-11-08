26677ce0243987bda37685e3fae99746c6542534
Paulo <paulowschaves@gmail.com>
2017-11-07 22:36:25 -0200

    Não deu certo o que queria mas já salva algumas coisas no banco rsrsr

--- app/src/main/java/com/wps/guiamedico/MapsActivity.java
+++ app/src/main/java/com/wps/guiamedico/MapsActivity.java
@@ -3,6 +3,10 @@ package com.wps.guiamedico;
 import android.content.Intent;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -12,19 +16,43 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
-{
+import java.util.Map;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private Float latitude;
     private Float longitude;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private FirebaseUser user;
+    private FirebaseAuth mAuth;
+    EditText texMessage;
+    TextView txTeste;
 
-
+    private DatabaseReference mDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        mAuth = FirebaseAuth.getInstance();
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        mDatabase = FirebaseDatabase.getInstance().getReference("pessoa");
+        texMessage = (EditText)findViewById(R.id.tx_message);
+        txTeste = (TextView) findViewById(R.id.tx_teste);
+
+        // Write a message to the database
+
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -32,18 +60,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         TextView textView;
         Intent it = getIntent();
 
-        textView = (TextView)findViewById(R.id.tx_local);
-        textView.setText((String)it.getStringExtra("nome"));
+        textView = (TextView) findViewById(R.id.tx_local);
+        textView.setText((String) it.getStringExtra("nome"));
 
-        textView = (TextView)findViewById(R.id.tx_telefone);
-        textView.setText((String)it.getStringExtra("telefone"));
+        textView = (TextView) findViewById(R.id.tx_telefone);
+        textView.setText((String) it.getStringExtra("telefone"));
 
-        textView = (TextView)findViewById(R.id.tx_endereco);
-        textView.setText((String)it.getStringExtra("endereco"));
+        textView = (TextView) findViewById(R.id.tx_endereco);
+        textView.setText((String) it.getStringExtra("endereco"));
 
-        latitude = Float.parseFloat((String)it.getStringExtra("latitude"));
-        longitude = Float.parseFloat((String)it.getStringExtra("longitude"));
+        latitude = Float.parseFloat((String) it.getStringExtra("latitude"));
+        longitude = Float.parseFloat((String) it.getStringExtra("longitude"));
 
+
+    }
+
+    public void buttonOnClick(View v) {
+        if(texMessage != null) {
+            mDatabase.child("comentario").setValue(texMessage.getText().toString());
+            Toast.makeText(MapsActivity.this,"Dados salvos com sucesso", Toast.LENGTH_SHORT).show();
+        }else{
+            Toast.makeText(MapsActivity.this,"Deu ruim!", Toast.LENGTH_SHORT).show();
+            texMessage.setText("");
+        }
     }
 
     @Override
